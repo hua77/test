57ff61414f47ab02f1610a6b75e3b81b45644003
hua77 <37231755+hua77@users.noreply.github.com>
2018-03-29 10:13:18 +0800

    对hello.java修改
    发现编译时错误，并进行修改

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
 public class hello{
-  public static main(String[] args){
-    system.out.println("hello world!");
+  public static void main(String[] args){
+    System.out.println("hello world!");
   }
 }
